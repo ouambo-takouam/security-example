ca002ed9886dd90a95ffc98e6806ca838d17db89
patrick ouambo <patrickouambo@gmail.com>
2023-03-28 17:02:13 +0200

    https on node.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+	return res.status(200).send('helooooo');
+});
+
+https
+	.createServer(
+		{
+			key: fs.readFileSync('key.pem'),
+			cert: fs.readFileSync('cert.pem'),
+		},
+		app
+	)
+	.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}`);
+	});
